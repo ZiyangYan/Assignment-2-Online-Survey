5d6107c0f3358c259af27126959e80fc51029480
YAN ZIYANG <21401063@life.hkbu.edu.hk>
2021-10-30 00:24:29 +0800

    adding AJAX to submit,delete and aginate

--- routes/index.js
+++ routes/index.js
@@ -18,18 +18,26 @@ function databaseInitialize() {
   }
 }
 
-/* Handle the Form */
-router.post('/form', function (req, res) {
+// /* Handle the Form */
+// router.post('/surveys', function (req, res) {
 
-  var response = {
-    header: req.headers,
-    body: req.body
-  };
+//   var response = {
+//     header: req.headers,
+//     body: req.body
+//   };
 
-  //req.body.numTickets = parseInt(req.body.numTickets);
-  db.getCollection("surveys").insert(req.body)
+//   //req.body.numTickets = parseInt(req.body.numTickets);
+//   db.getCollection("surveys").insert(req.body)
 
-  res.json(response);
+//   res.json(response);
+// });
+
+/* Handle the Form submission with Restful Api */
+router.post('/surveys', function (req, res) {
+
+  let result = db.getCollection("surveys").insert(req.body);
+
+  res.status(201).json({ id: result.$loki });
 });
 
 /* GET home page. */
@@ -60,18 +68,24 @@ router.get('/surveys/read/:id', function (req, res) {
 });
 
 // Delete a single Survey 
-router.post('/surveys/delete/:id', function (req, res) {
+router.delete('/surveys/:id', function (req, res) {
 
-  // db.getCollection("bookings").findAndRemove({ $loki: parseInt(req.params.id) });
+  //db.getCollection("surveys").findAndRemove({ $loki: parseInt(req.params.id) });
 
   let result = db.getCollection("surveys").findOne({ $loki: parseInt(req.params.id) });
-  
+
   if (!result) return res.status(404).send('Unable to find the requested resource!');
- 
+
   db.getCollection("surveys").remove(result);
-  
-  res.send("survey deleted.");
-   
+
+  // //res.send("survey deleted.");
+  // res.redirect("/surveys");
+  if (req.get('Accept').indexOf('html') === -1) {
+    return res.status(204).send();	    // for ajax request
+  } else {
+    return res.redirect('/surveys');	// for normal HTML request
+  }
+
 });
 
 /* Searching */
@@ -107,6 +121,25 @@ router.get('/surveys/paginate', function (req, res) {
 
   return res.render('paginate', { surveys: results, numOfRecords: totalNumRecords });
 
+});
+
+/* Ajax Pagination */
+router.get('/surveys/aginate', function (req, res) {
+  if (req.get('Accept').indexOf('html') === -1) {
+
+    var count = Math.max(req.query.limit, 2) || 2;
+    var start = Math.max(req.query.offset, 0) || 0;
+
+    var results = db.getCollection("surveys").chain().find({}).offset(start).limit(count).data();
+
+    return res.json(results);
+
+  } else {
+
+    var totalNumRecords = db.getCollection("surveys").count();
+
+    return res.render('aginate', { numOfRecords: totalNumRecords });
+  }
 });
 
 module.exports = router;
